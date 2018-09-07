279c22ae76b8ca56edc72568fcff414f2fb1defa
Nguyen The Thuc <thucfami@gmail.com>
2018-09-07 12:47:13 +0700

    layout

--- src/Thuc/Zend/ControllerTrait.php
+++ src/Thuc/Zend/ControllerTrait.php
@@ -7,11 +7,12 @@ Trait ControllerTrait {
     protected $dm;
     protected $code;
     protected $ENV;
-    
+
     public function __construct($dm, $ENV) {
         $this->dm = $dm;
         $this->code = new \Thuc\Zend\Code($this);
         $this->ENV = $ENV;
+        $this->layout()->ENV = $ENV;
     }
 
 }
