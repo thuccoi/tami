d57f2d68bb230bd2d56409ccb69d0a1877cf21fa
Nguyen The Thuc <thucfami@gmail.com>
2018-09-21 15:54:33 +0700

    admin

---
+++ src/Thuc/Zend/AdminController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Thuc\Zend;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Mvc\MvcEvent;
+
+class AdminController extends AbstractActionController {
+
+    use \Thuc\Zend\ControllerTrait;
+
+    public function onDispatch(MvcEvent $e) {
+        // Call the base class' onDispatch() first and grab the response
+        $response = parent::onDispatch($e);
+
+        // Set alternative layout
+        $this->layout()->setTemplate('admin/layout');
+        $this->layout()->ENV = $this->ENV;
+
+        if (!$this->sessionContainer || !isset($this->sessionContainer->viewer)) {
+            return $this->redirect()->toRoute("outside", ["action" => "dang-nhap"]);
+        }
+
+        $this->layout()->viewer = $this->sessionContainer->viewer;
+
+        // Return the response
+        return $response;
+    }
+
+    public function indexAction() {
+
+        return [
+        ];
+    }
+    
+    public function hoSoAction(){
+        return [];
+    }
+
+}
